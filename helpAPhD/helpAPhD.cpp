9c832d7bc3252916b558f52b2c50519793b1c2db
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-09-29 13:31:56 -0600

    started helpAPhD, retabbed mapTiles, started pickUpSticks stuck on toposort

---
+++ helpAPhD/helpAPhD.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+  int cases, num1, num2;
+  string prob, currNum;
+  bool first=true;
+
+  cin >> cases;
+
+  for (int i = 0; i < cases; ++i) {
+    cin >> prob;
+    if (prob == "P=NP") {
+      cout << "skipped" << endl;
+    }
+    else{
+      for (int j = 0; j < prob.length(); ++j) {
+        if (prob.at(j) == '+' && first) {
+          num1 = atoi(currNum.c_str());
+        }
+      }
+    }
+  }
+  return 0;
+}
