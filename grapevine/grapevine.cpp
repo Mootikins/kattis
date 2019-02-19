295af539400d7ed718f870f4a7acee44f326ef3a
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-02-19 14:08:22 -0700

    started grapevine

---
+++ grapevine/grapevine.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+struct person {
+	int skepticism;
+	std::string name;
+};
+
+int main(int argc, char *argv[])
+{
+	int num_people, connections, days;
+	std::cin >> num_people >> connections >> days;
+
+	return 0;
+}
