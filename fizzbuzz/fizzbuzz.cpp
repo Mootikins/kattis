fd4d8780e0330072362062c20ea9fe4fa2aa9eaf
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-03-27 11:24:35 -0600

    solved fizzbuzz

---
+++ fizzbuzz/fizzbuzz.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+int main(int argc, const char *argv[])
+{
+	int div1, div2, upperBound;
+	std::cin >> div1 >> div2 >> upperBound;
+
+	for (int i = 1; i <= upperBound; i++) {
+		std::string output;
+		if ( !( i % div1 ) ) {
+			output += "Fizz";
+		}
+		if ( !( i % div2 ) ) {
+			output += "Buzz";
+		}
+		if ( output.empty() ) {
+			std::cout << i << std::endl;
+		}
+		else {
+			std::cout << output << std::endl;
+		}
+	}
+
+	return 0;
+}
