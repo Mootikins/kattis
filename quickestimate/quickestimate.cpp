ddd2831478c24cbce4fd82056361cd04b58cd201
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-10-09 18:12:48 -0600

    Problems from a practice

---
+++ quickestimate/quickestimate.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	string num;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		cout << num.length() << endl;
+	}
+
+	return 0;
+}
