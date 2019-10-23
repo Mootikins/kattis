24192705c7e6cab4ec04beda25c31b7edd6971fa
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-10-22 18:08:56 -0600

    pearwise and change to makeDir.sh

--- makeDir.sh
+++ makeDir.sh
@@ -3,5 +3,5 @@ PWD=$(pwd)
 mkdir $1
 cd $1
 cp ../template.mk ./Makefile
-printf "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main()\n{\n\n\treturn 0;\n}" > "$1".cpp
-(cd "$PWD"; $VISUAL "$1".cpp +':silent exec "YcmGenerateConfig"')
+printf "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main() {\n\treturn 0;\n}" > "$1".cpp
+(cd "$PWD"; make; $VISUAL "$1".cpp +':silent exec "YcmGenerateConfig"')
